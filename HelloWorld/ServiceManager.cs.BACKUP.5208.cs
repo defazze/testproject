bb81b2129a7184f039d891604a9670cfa3585dbe
glagolev <glagolev@astralnalog.ru>
2015-10-07 10:23:00 +0300

    Инициируем конфликт master

---
+++ HelloWorld/ServiceManager.cs.BACKUP.5208.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class ServiceManager
+    {
+        public void Add()
+        {
+            //Напоняем метод Add
+<<<<<<< HEAD
+            //Feature in dev1 branch
+=======
+            //Тут будет конфликт
+>>>>>>> Инициируем конфликт master
+        }
+    }
+}
---
+++ HelloWorld/ServiceManager.cs.LOCAL.5208.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class ServiceManager
+    {
+        public void Add()
+        {
+            //Напоняем метод Add
+            //Feature in dev1 branch
+        }
+    }
+}
