bd09f64b013f43fbcdbf586a0f1527f1d846d2b0
glagolev <glagolev@astralnalog.ru>
2015-10-07 10:23:00 +0300

    Инициируем конфликт master

---
+++ HelloWorld/ServiceManager.cs.BASE.5208.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class ServiceManager
+    {
+        public void Add()
+        {
+            //Напоняем метод Add
+        }
+    }
+}
